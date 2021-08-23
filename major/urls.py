fa1d1b4367cafb247c60dea874c64a759818cc89
Soohyun <csh0sns@naver.com>
2021-08-23 18:08:10 +0900

    major view 캡슐화/major urls 끝

--- major/urls.py
+++ major/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "major"
 
 urlpatterns = [
+    path("food", views.food_list, name="food_list"),
+    path("clothing", views.clothing_list, name="clothing_list"),
     path("consumer", views.consumer_list, name="consumer_list"),
+    path("child", views.child_list, name="child_list"),
 ]
--- major/views.py
+++ major/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render
 from major.models import MajorPost
 
 
-def consumer_list(request):
-    queryset = MajorPost.objects.filter(major="consumer")
+def get_ctx(major_en, major_ko):
+    queryset = MajorPost.objects.filter(major=major_en)
     ctx = {
-        "major": "소비자학과",
+        "major": major_ko,
         "posts": queryset,
     }
+    return ctx
+
+
+def food_list(request):
+    ctx = get_ctx("food", "식품영양학과")
+    return render(request, "major/major_list.html", ctx)
+
+
+def clothing_list(request):
+    ctx = get_ctx("clothing", "의류학과")
+    return render(request, "major/major_list.html", ctx)
+
+
+def consumer_list(request):
+    ctx = get_ctx("consumer", "소비자학과")
+    return render(request, "major/major_list.html", ctx)
+
+
+def child_list(request):
+    ctx = get_ctx("child", "아동가족학과")
     return render(request, "major/major_list.html", ctx)
