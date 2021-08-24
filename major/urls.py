60c086c3967b93fbef03260ab01385f6413c96a4
Soohyun <csh0sns@naver.com>
2021-08-24 19:38:17 +0900

    major_detail view/template 완성

--- major/urls.py
+++ major/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("clothing", views.clothing_list, name="clothing_list"),
     path("consumer", views.consumer_list, name="consumer_list"),
     path("child", views.child_list, name="child_list"),
+    path("<int:pk>", views.major_detail, name="major_detail"),
 ]
--- major/views.py
+++ major/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from major.models import MajorPost
 
 
-def get_ctx(major_en, major_ko):
+def get_list_ctx(major_en, major_ko):
     queryset = MajorPost.objects.filter(major=major_en)
     ctx = {
         "major": major_ko,
@@ -11,21 +11,39 @@ def get_ctx(major_en, major_ko):
     return ctx
 
 
+def get_major(major_en):
+    major_dict = {"food": "식품영양학과",
+                  "clothing": "의류학과",
+                  "consumer": "소비자학과",
+                  "child": "아동가족학과"}
+    return major_dict[major_en]
+
+
 def food_list(request):
-    ctx = get_ctx("food", "식품영양학과")
+    ctx = get_list_ctx("food", "식품영양학과")
     return render(request, "major/major_list.html", ctx)
 
 
 def clothing_list(request):
-    ctx = get_ctx("clothing", "의류학과")
+    ctx = get_list_ctx("clothing", "의류학과")
     return render(request, "major/major_list.html", ctx)
 
 
 def consumer_list(request):
-    ctx = get_ctx("consumer", "소비자학과")
+    ctx = get_list_ctx("consumer", "소비자학과")
     return render(request, "major/major_list.html", ctx)
 
 
 def child_list(request):
-    ctx = get_ctx("child", "아동가족학과")
+    ctx = get_list_ctx("child", "아동가족학과")
     return render(request, "major/major_list.html", ctx)
+
+
+def major_detail(request, pk):
+    queryset = MajorPost.objects.get(pk=pk)
+    major = get_major(queryset.major)
+    ctx = {
+        "major": major,
+        "post": queryset,
+    }
+    return render(request, "major/major_detail.html", ctx)
