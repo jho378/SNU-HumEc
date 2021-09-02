7ef2f40cbfa0c16fa79266193ee26eea45ff947f
Soohyun <csh0sns@naver.com>
2021-09-02 21:48:22 +0900

    notice admin 등록

--- major/admin.py
+++ major/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from major.models import MajorPost
-from accounts.models import User
 
 admin.site.register(MajorPost)
-admin.site.register(User)
--- notice/views.py
+++ notice/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Notice
 
 
 def notice_list(request):
-    return render(request, "notice/notice_list.html")
+    queryset = Notice.objects.all()
+    ctx = {
+        "posts": queryset,
+    }
+    return render(request, "notice/notice_list.html", ctx)
