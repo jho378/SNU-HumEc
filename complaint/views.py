6b0a24a0e6982c0af76639a5706c2a81935011ed
Soohyun <csh0sns@naver.com>
2021-09-05 20:56:59 +0900

    complaint comment 양식 재제출 방지

--- complaint/views.py
+++ complaint/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Complaint, ComplaintComment
 
 
@@ -15,17 +17,20 @@ def complaint_list(request):
 def complaint_detail(request, pk):
     queryset = Complaint.objects.get(pk=pk)
     comments = ComplaintComment.objects.filter(post=queryset)  # .get() 함수는 하나의 object return
-    is_post_user = True if queryset.user == request.user else False
+    login_user = request.user
+    is_post_user = True if queryset.user == login_user else False
 
     if request.method == "POST":
         post = queryset
         contents = request.POST["contents"]
         user = request.user
         ComplaintComment.objects.create(post=post, contents=contents, user=user)
+        return redirect("complaint:complaint_detail", pk)  # 양식 재제출 방지 (새로고침 시 마지막 작성 댓글 재작성 막기)
 
     ctx = {
         "post": queryset,
         "comments": comments,
         "is_post_user": is_post_user,
+        "login_user": login_user,
     }
     return render(request, "complaint/complaint_detail.html", ctx)
