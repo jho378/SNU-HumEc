2fa2cfbd1b34c052c2288f96a756f8cb287ae00a
Soohyun <csh0sns@naver.com>
2021-09-14 23:47:12 +0900

    community post model 생성

--- community/models.py
+++ community/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class CommunityPost(models.Model):
+    board = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    image = models.ImageField(blank=True, upload_to="community/image")
+    file = models.ImageField(blank=True, upload_to="community/file")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hits = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.pk}: {self.title}"
+
+    @property
+    def click(self):  # 조회수
+        self.hits += 1
+        self.save()
