27879f4482a2903c64ae02dba2041be86037aba8
Soohyun <csh0sns@naver.com>
2021-09-15 20:45:03 +0900

    community_detail.html href 수정 / CommunityComment model admin 등록

--- community/admin.py
+++ community/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import CommunityPost
+from .models import CommunityPost, CommunityComment
 
 admin.site.register(CommunityPost)
+admin.site.register(CommunityComment)
