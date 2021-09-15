7ee732a0d1948fa71859b6ae110d3e50be7168b6
Soohyun <csh0sns@naver.com>
2021-09-15 16:25:38 +0900

    community list 생성

--- community/urls.py
+++ community/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "community"
 
 urlpatterns = [
-    path("market/", views.community_market, name="community_market"),
+    path("market/", views.market_list, name="market_list"),
+    path("promotion/", views.promotion_list, name="promotion_list"),
+    path("free/", views.free_list, name="free_list"),
+    path("study/", views.study_list, name="study_list"),
 ]
--- community/views.py
+++ community/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from .models import CommunityPost
 
 
-def community_market(request):
-    return render(request, "community/community_list.html")
+def get_board(board_en):
+    board_dict = {"market": "장터 게시판",
+                  "promotion": "홍보 게시판",
+                  "free": "자유 게시판",
+                  "study": "스터디/강의 게시판"}
+    return board_dict[board_en]
+
+
+def get_list_ctx(board_en, board_ko):
+    queryset = CommunityPost.objects.filter(board=board_en)
+    ctx = {
+        "board": board_ko,
+        "posts": queryset,
+    }
+    return ctx
+
+
+def market_list(request):
+    ctx = get_list_ctx("market", "장터 게시판")
+    return render(request, "community/community_list.html", ctx)
+
+
+def promotion_list(request):
+    ctx = get_list_ctx("promotion", "홍보 게시판")
+    return render(request, "community/community_list.html", ctx)
+
+
+def free_list(request):
+    ctx = get_list_ctx("free", "자유 게시판")
+    return render(request, "community/community_list.html", ctx)
+
+
+def study_list(request):
+    ctx = get_list_ctx("study", "스터디/강의 게시판")
+    return render(request, "community/community_list.html", ctx)
