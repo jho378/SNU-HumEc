900b27a5f4ad905cb0a114d5a6f25ae33a5e5a33
Soohyun <csh0sns@naver.com>
2021-09-16 17:38:38 +0900

    notice create pin checkbox 구현

--- notice/views.py
+++ notice/views.py
@@ -4,9 +4,11 @@ from .forms import NoticeForm
 
 
 def notice_list(request):
-    queryset = Notice.objects.all()
+    queryset = Notice.objects.filter(pin=False)
+    pin_notice = Notice.objects.filter(pin=True)
     ctx = {
         "posts": queryset,
+        "pin_posts": pin_notice,
     }
     return render(request, "notice/notice_list.html", ctx)
 
@@ -26,6 +28,8 @@ def notice_create(request):
         form = NoticeForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            is_pin = request.POST.getlist("pin")  # form 내 해당하는 name의 value를 list 형태로 받아옴
+            post.pin = True if is_pin else False
             post.user = request.user
             post.save()
             return redirect("notice:notice_list")
@@ -39,15 +43,21 @@ def notice_create(request):
 
 def notice_update(request, pk):
     queryset = Notice.objects.get(pk=pk)
+    is_pin = True if queryset.pin else False
+
     if request.method == "POST":
         form = NoticeForm(request.POST, instance=queryset)
         if form.is_valid():
+            post = form.save(commit=False)
+            is_pin = request.POST.getlist("pin")
+            post.pin = True if is_pin else False
             form.save()
             return redirect("notice:notice_detail", pk)
     else:
         form = NoticeForm(instance=queryset)
     ctx = {
         "form": form,
+        "is_pin": is_pin,
     }
     return render(request, "notice/notice_create.html", ctx)
 
