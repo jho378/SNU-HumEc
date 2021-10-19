de829c562e6dab9095f202ac6f5af2eb2b34cf4e
Soohyun <csh0sns@naver.com>
2021-10-19 23:44:49 +0900

    base.html council url 연결

--- council/urls.py
+++ council/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("about/", views.council_about, name="council_about"),
     path("rule/", views.council_rule, name="council_rule"),
     path("conference/", views.council_conference, name="council_conference"),
-    path("minutes/", views.council_minutes, name="council_minutes)"),
+    path("minutes/", views.council_minutes, name="council_minutes"),
 ]
